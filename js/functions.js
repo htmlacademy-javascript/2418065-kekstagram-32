9f8f35e962044d242173af5a1b419c4432bfa2e8
NovikoffArtem <142929498+NovikoffArtem@users.noreply.github.com>
2024-06-24 16:42:39 +0300

    нужно больше функций

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+// Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.
+
+const checkStringLenght = function (string, maxLength) {
+  return string.length <= maxLength;
+};
+
+// Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
+
+const checkStringIsPalindrome = function (string) {
+  let newString = '';
+  string = string.replaceAll(' ', '').toLowerCase();
+  for(let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return string === newString ;
+};
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+const getNumber = function (string) {
+  if(typeof(string) === 'number') {
+    string = string.toString();
+  }
+  let number = '';
+  for(let i = 0; i < string.length; i++) {
+    let symbol = string[i];
+    symbol = parseInt(symbol, 10);
+    number += Number.isNaN(symbol) ? '' : symbol;
+  }
+  return parseInt(number, 10);
+};
+
